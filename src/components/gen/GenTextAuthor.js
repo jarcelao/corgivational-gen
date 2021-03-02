c88058fb5731626a3b818bb517d1a7bedd5729b6
Szel Embay <68631433+Hayzhel@users.noreply.github.com>
2021-03-02 09:48:36 +0800

    lowered the author text a bit
    just adjusted how the author text so it's not as close to the quote

--- src/components/gen/GenTextAuthor.js
+++ src/components/gen/GenTextAuthor.js
@@ -6,7 +6,7 @@ class GenTextAuthor extends React.Component {
     render() {
         return (
             <Textfit mode="single" 
-                    className="absolute top-1/3 left-1/3 w-1/6 pt-3 pl-6 h-12 text-white font-arial text-outline author">
+                    className="absolute top-1/3 left-1/3 w-1/6 pt-6 pl-6 h-12 text-white font-arial text-outline author">
                 {this.props.text}
             </Textfit> 
         )
